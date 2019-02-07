443d2a567656ab3d946fb6ff922d99b03e8a1caa
Ryan Moore <44604941+Mooresy17@users.noreply.github.com>
2019-02-07 12:59:08 +0000

    Add files via upload

--- Frame.java
+++ Frame.java
@@ -1,47 +1,43 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
+package zbackgammon;
+
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
+import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-import java.awt.geom.*;
 
-public class Frame extends JComponent{
-	
-	class circle{
-	public void paintComponent(Graphics g){
-		// Recover Graphics2D 
-	      Graphics2D g2 = (Graphics2D) g;
-	      g2.setColor(Color.GREEN);
-	      Ellipse2D.Double ellipse = new Ellipse2D.Double(10,20,5,10);
-		  g2.draw(ellipse);
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-}
+public class Frame extends JPanel {
+
+	private Image backgroundImage;
+
+	// Some code to initialize the background image.
+	// Here, we use the constructor to load the image. This
+	// can vary depending on the use case of the panel.
+	public Frame(String fileName) throws IOException {
+		    backgroundImage = ImageIO.read(new File(fileName));
+		  }
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		// Draw the background image.
+		g.drawImage(backgroundImage, 5, 10, this);
 	}
 
-	
-	public static void main(String[] args){
-		JFrame frame = new JFrame();
-		frame.setSize(600, 450);
-		frame.setTitle("Backgammon");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		frame.add(new JLabel(new ImageIcon("src/image/Screen Shot 2019-02-05 at 21.04.05.png")));
-		
-		frame.setVisible(true);
-		Frame component = new Frame();
-		frame.add(component);
-		
-		
-		
-		
-		
-        
-		
-	}
 
+public static void main(String[] args) throws IOException{
+	JFrame frame = new JFrame();
+    frame.setSize(600, 450);
+    frame.setTitle("Backgammon");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Frame component = new Frame("src/image/Screen Shot 2019-02-05 at 21.04.05.png");
+    frame.add(component);
+    frame.setVisible(true);
 }
+}
+
+
+
