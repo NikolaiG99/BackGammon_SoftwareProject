9398d96faea2055d5f4dee188fcee52ef1025391
Ryan Moore <44604941+Mooresy17@users.noreply.github.com>
2019-03-19 17:34:38 +0000

    Extra coordinates for cheat bar positions

--- src/graphical_display/BoardCoordinateConstants.java
+++ src/graphical_display/BoardCoordinateConstants.java
@@ -12,28 +12,28 @@ public class BoardCoordinateConstants{
 	 */
 	public final static int [] COORDS_POINT
 		= new int []{
-				580, 530, 490, 445, 400, 360, 316, 240, 198, 155,115, 70, 25,
-				25, 70, 115, 155, 198, 240, 316, 360, 400, 445, 490, 530, 580, 278, 278
+				575, 530, 490, 445, 400, 360, 316, 240, 198, 155,115, 70, 25,
+				25, 70, 115, 155, 198, 240, 316, 360, 400, 445, 490, 530, 575, 278, 278
 		}; 
 	
 	/**
 	 * An array of the y-coordinates for each row on the top of the board.
 	 * This array stores data in elements [1] to [6], where 1 is the top most row
-	 * [0] is bear off/bar for red
+	 * [0] is bear off/bar for red, 7-9 are the bar for cheat
 	 */
 	public final static int [] COORDS_TOP_ROW
 	= new int []{
-			170, 25, 55, 85, 115, 145, 175
+			170, 25, 55, 85, 115, 145, 175, 140, 110, 80
 	}; 
 	
 	/**
 	 * An array of the y-coordinates for each row on the bottom of the board.
 	 * This array stores data in elements [1] to [6], where 1 is the bottom most row
-	 * [0] is bear off/bar for black
+	 * [0] is bear off/bar for black, 7-9 are the bar for cheat
 	 */
 	public final static int [] COORDS_BOTTOM_ROW
 	= new int []{
-			208, 362, 332, 302, 272, 242, 212
+			208, 362, 332, 302, 272, 242, 212, 238, 268, 298
 	}; 
 	
 	public final static int MIDDLE_ROW = 193;
